4a81f1c12f9babe616b496b6846afa69262fdcfc
Misha <myhaylo03@gmail.com>
2021-06-08 10:25:00 +0300

    remove secrets

--- BasketballWepApi/Migrations/20210608072343_New table.Designer.cs
+++ BasketballWepApi/Migrations/20210608072343_New table.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApi.Context;
 
 namespace BasketballWepApi.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210608072343_New table")]
+    partial class Newtable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
